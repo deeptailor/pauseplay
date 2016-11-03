d1833be6d6859e882fcb929bdb91442703cd5e8d
Deep Tailor <deeptailor@Deeps-MacBook-Pro.local>
2016-11-03 09:29:32 -0700

    guest seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+guest = {username:'guest',
+  password:'password',
+  description:'guest account',
+  profile_image_url:'https://res.cloudinary.com/deeptailor/image/upload/v1478189787/guest_le1jtm.png'
+}
+
+User.create(guest);
