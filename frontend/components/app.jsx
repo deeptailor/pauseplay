9d9512c02645b8d845d213fe0c215673a19e6cdd
Deep Tailor <deeptailor@Deeps-MacBook-Pro.local>
2016-11-04 20:00:36 -0700

    quick changes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,7 +6,6 @@ const App = ({ children }) => (
   <div className="app-container">
     <GreetingContainer/>
     {children}
-    <AudioPlayer/>
   </div>
 );
 
--- frontend/components/audio-player/audio-player.jsx
+++ frontend/components/audio-player/audio-player.jsx
@@ -17,7 +17,6 @@ class AudioPlayer extends React.Component{
           height={"4vh"}
           width = {"80%"}
           backgroundColor={"black"}
-          controls={true}
           url='https://res.cloudinary.com/dwf6beu4e/video/upload/v1473126400/wi8dtbavpe7f11yccu5t.mp3'
           />
       </div>
