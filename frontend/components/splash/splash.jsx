919a18b451c196128f9b3a7045f3b4448afaab1a
Deep Tailor <deeptailor@Deeps-MacBook-Pro.local>
2016-11-11 15:48:45 -0800

    last changes

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -51,7 +51,6 @@ class Splash extends React.Component{
           </div>
 
           <div className="splash-album-container">
-            <div className="whats-new-splash" onClick={(e)=>window.scrollBy(0,window.innerHeight)}><i className="material-icons">keyboard_arrow_down</i></div>
             <ul className="splash-albums-ul">
             {this.renderAlbums()}
             </ul>
